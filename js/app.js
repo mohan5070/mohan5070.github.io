63863bd52513345c803bac3bb883c7b2498e85f0
Mohan <mohan@cloudthing.com>
2017-07-18 18:43:34 +0530

    slider loading issue fixed

--- js/app.js
+++ js/app.js
@@ -3,6 +3,29 @@ $(function() {
     var $pageDom = $('#page-content-wrapper');
     var $listItems = $('.sidebar-nav li');
 
+
+    function loadPortfolio() {
+        $('.portfolio-slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            rows:2
+        });
+    }
+
+    function loadBlog() {
+        $(".blog-slider").slick({
+            centerMode: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            dots: true,
+            customPaging : function(slider, i) {
+                debugger;
+                var thumb = $(slider.$slides[i]).find('img').attr('src');
+                return '<a><img src="'+thumb+'" width="20px"></a>';
+            },
+        });
+    }
+
     $listItems.click(function(e) {
         var currentDom = $(e.currentTarget);
         var pageName = currentDom.data('page');
@@ -10,23 +33,16 @@ $(function() {
         currentDom.addClass('active');
         $('.row').addClass('hide')
         $('.'+pageName+'-content').removeClass('hide');
+
+        if(pageName === 'portfolio') {
+            loadPortfolio();
+        }
+
+         if(pageName === 'blog') {
+            loadBlog();
+        }
     });
 
-    $('.portfolio-slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        rows:2
-    });
 
-    $(".blog-slider").slick({
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dots: true,
-        customPaging : function(slider, i) {
-            var thumb = $(slider.$slides[i]).data('thumb');
-            return '<a><img src="'+thumb+'"></a>';
-        },
-    });
 
 });
